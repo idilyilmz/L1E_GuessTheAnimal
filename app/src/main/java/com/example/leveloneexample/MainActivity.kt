6eeb17697af0cb747615086e0e85a118effa9acc
IdilYilmaz <Idil.yilmaz@hva.nl>
2025-02-03 11:00:11 +0100

    optimized imports

--- app/src/main/java/com/example/leveloneexample/MainActivity.kt
+++ app/src/main/java/com/example/leveloneexample/MainActivity.kt
@@ -13,12 +13,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.leveloneexample.ui.theme.LevelOneExampleTheme
 
 class MainActivity : ComponentActivity() {
