232c6be94c25b96bd86199cc7871e600bf2ea0dd
IdilYilmaz <Idil.yilmaz@hva.nl>
2025-02-03 10:36:14 +0100

    set up the skeleton of my app

--- app/src/main/java/com/example/leveloneexample/MainActivity.kt
+++ app/src/main/java/com/example/leveloneexample/MainActivity.kt
@@ -6,10 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.leveloneexample.ui.theme.LevelOneExampleTheme
 
@@ -19,29 +26,52 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LevelOneExampleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Idil",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GuessAnimalScreen()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api:: class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun GuessAnimalScreen() {
+   Scaffold(
+       topBar = {
+           TopAppBar(
+               title = {
+                   Text (
+                       text = stringResource(id = R.string.app_name)
+                   )
+               },
+               colors = TopAppBarDefaults.topAppBarColors(
+                   containerColor = MaterialTheme.colorScheme.primary,
+                   titleContentColor = MaterialTheme.colorScheme.onPrimary
+               )
+           )
+       },
+       content = { padding -> ScreenContent(Modifier.padding(padding))},
+   )
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier) {
+    // TODO: we add more code here later
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        "It works! - remove this text later",
+        modifier = modifier.padding()
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun GuessAnimalScreenPreview() {
     LevelOneExampleTheme {
-        Greeting("Yilmaz")
+        GuessAnimalScreen()
     }
 }
