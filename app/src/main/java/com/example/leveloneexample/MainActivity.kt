5a9ba0c729a049d4d6b1e65aa39c90109e907a51
IdilYilmaz <Idil.yilmaz@hva.nl>
2025-02-03 10:39:57 +0100

    added space on the inside of a ui element

--- app/src/main/java/com/example/leveloneexample/MainActivity.kt
+++ app/src/main/java/com/example/leveloneexample/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.leveloneexample.ui.theme.LevelOneExampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -61,11 +62,9 @@ fun GuessAnimalScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier) {
-    // TODO: we add more code here later
-    Text(
-        "It works! - remove this text later",
-        modifier = modifier.padding()
-    )
+    //TODO: we add more code here later
+    Text(modifier = modifier.padding(16.dp),
+        text = "It works! - remove this text later")
 }
 
 @Preview(showBackground = true)
