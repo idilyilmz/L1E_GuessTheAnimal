d9006f53f55738d64438737002502246683f3d5d
IdilYilmaz <Idil.yilmaz@hva.nl>
2025-02-04 14:24:17 +0100

    user can now enter a character

--- app/src/main/java/com/example/leveloneexample/MainActivity.kt
+++ app/src/main/java/com/example/leveloneexample/MainActivity.kt
@@ -32,6 +32,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 
 import com.example.leveloneexample.ui.theme.LevelOneExampleTheme
@@ -54,29 +57,33 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api:: class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GuessAnimalScreen() {
-   Scaffold(
-       topBar = {
-           TopAppBar(
-               title = {
-                   Text (
-                       text = stringResource(id = R.string.app_name)
-                   )
-               },
-               colors = TopAppBarDefaults.topAppBarColors(
-                   containerColor = MaterialTheme.colorScheme.primary,
-                   titleContentColor = MaterialTheme.colorScheme.onPrimary
-               )
-           )
-       },
-       content = { padding -> ScreenContent(Modifier.padding(padding))},
-   )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.app_name)
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary
+                )
+            )
+        },
+        content = { padding -> ScreenContent(Modifier.padding(padding)) },
+    )
 }
 
 @Composable
 fun ScreenContent(modifier: Modifier) {
+
+    // Local variable to hold and save the value of the text entered by the user.
+    var answerText by remember { mutableStateOf(String()) }
+
     Column(
         modifier
             .fillMaxHeight()
@@ -99,10 +106,10 @@ fun ScreenContent(modifier: Modifier) {
             verticalAlignment = Alignment.CenterVertically
         ) {
             OutlinedTextField(
-                value = String(),
+                value = answerText,
                 placeholder = { Text(text = stringResource(id = R.string.animal_question)) },
                 onValueChange = {
-                    // TODO to be completed
+                    answerText = it
                 },
                 label = { Text(stringResource(R.string.answer_label)) }
             )
